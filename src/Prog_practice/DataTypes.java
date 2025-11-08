2b232e26982d112539c357e3b3f8d5be40fb37b0
maheshmorebe <116181700+maheshmorebe@users.noreply.github.com>
2025-11-08 17:53:18 +0530

    Create DataTypes.java
    Added datatypes.java file directly in github folder

---
+++ src/Prog_practice/DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+
+
+	public static void main(String args[]){
+
+	String name = "Mahesh";
+	int age = 74;
+	long sal = 50000000;
+	float duration = 65.5f;
+	double price = 249.892;
+	char clas = 'A';
+	boolean isLikeJava = true;
+	byte height = 2;
+	short weight = 150;
+
+	System.out.println("My name is "+name+" and my age is "+age+" having salary "+sal+".");
+
+	System.out.println("The price of the song's CD is "+price+" Rs. which is having "+clas+" one songs of duration "+duration+" minutes.");
+	System.out.println("Do you like Java? ->"+isLikeJava);
+
+	System.out.println("My name is "+name+". My weight and height is "+weight+" Kg. and "+height+" Feet respectively.");
+
+	}
+
+}
